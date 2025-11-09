e41077d8b46744585e7980a768569177ac0a5946
Nikita Shibaev <nshib.54321@gmail.com>
2025-11-09 12:55:53 +0300

    Update refresh token model

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 
 
 class RefreshToken(models.Model):
-    token = models.CharField(max_length=255, unique=True)
+    jti = models.CharField(max_length=36, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     expires_at = models.DateTimeField()
     user = models.ForeignKey(
