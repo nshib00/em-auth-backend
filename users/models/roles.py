2b36256433525440574775888d97ce7885bf2940
Nikita Shibaev <nshib.54321@gmail.com>
2025-11-11 16:47:03 +0300

    Add is_admin field in Role model

--- users/models/roles.py
+++ users/models/roles.py
@@ -19,3 +19,13 @@ class Role(models.Model):
     description = models.TextField(blank=True)
     is_default = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['is_admin'],
+                condition=models.Q(is_admin=True),
+                name='only_one_admin_role'  # роль администратора может быть только одна на всю БД
+            )
+        ]
