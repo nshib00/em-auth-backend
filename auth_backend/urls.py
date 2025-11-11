db3e0e2ed91995f41bd4e5606bef159dbcf6878d
Nikita Shibaev <nshib.54321@gmail.com>
2025-11-11 09:24:59 +0300

    Add api/ prefix for all DRF endpoints

--- auth_backend/urls.py
+++ auth_backend/urls.py
@@ -3,12 +3,17 @@ from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
-urlpatterns = [
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
-
-    path('admin/', admin.site.urls),
+api_urls = [
+    path('users/', include('users.urls')),
     path('auth/', include('jwt_auth.urls')),
     # path('objects/', include('mock_objects.urls')),
-    path('api/users/', include('users.urls')),
+
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
+]
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(api_urls)),
 ]
