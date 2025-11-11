a11c418696d770be41d9890d821de05f66dc3bf8
Nikita Shibaev <nshib.54321@gmail.com>
2025-11-11 16:51:41 +0300

    Create permission models

--- permissions/models.py
+++ permissions/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from users.models.roles import Role
+
+
+class ResourceType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        db_table = 'resource_types'
+
+
+class Action(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        db_table = 'actions'
+
+
+class Permission(models.Model):
+    role = models.ForeignKey(Role, on_delete=models.PROTECT)
+    resource_type = models.ForeignKey(ResourceType, on_delete=models.PROTECT)
+    action = models.ForeignKey(Action, on_delete=models.PROTECT)
+    
+    class Meta:
+        db_table = 'permissions'
+        unique_together = ['role', 'resource_type', 'action']
+    
+    def __str__(self):
+        return f'{self.role.name}_{self.resource_type.name}_{self.action.name}'
